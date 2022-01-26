f2dc8c034a408f246ad9d1ed76ffe1baf6b4a49a
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-01-26 17:12:48 +0600

    Final92

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -16,7 +16,7 @@ function App() {
             <AppFilter/>
         </div>
 
-        <EmployeesList/>
+        <EmployeesList />
         <EmployeesAddForm/>
     </div>
   );
--- src/components/employees-list/employees-list.tsx
+++ src/components/employees-list/employees-list.tsx
@@ -1,15 +1,14 @@
 import EmployeesListItem from "../employees-list-item/employees-list-item";
-
 import './employees-list.css';
 
 const EmployeesList = () => {
     return (
-        <ul className="app-list list-group">
-            <EmployeesListItem/>
-            <EmployeesListItem/>
-            <EmployeesListItem/>
+        <ul className='app-list list-group'>
+            <EmployeesListItem name= 'Homer S.' salary={500} increase={true}/>
+            <EmployeesListItem name= 'Lisa S.' salary= {5000} increase={false}/>
+            <EmployeesListItem name= 'Bart S.' salary= {100} increase={true} />
         </ul>
-    )
-}
 
+)
+}
 export default EmployeesList;
